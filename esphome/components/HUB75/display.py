8ff3c06177cd03fd14352aab4d95ab5a8eeec546
IJssel Koster <ij.koster@student.tudelft.nl>
2024-06-16 10:38:59 +0200

    Setup display.py

--- esphome/components/HUB75/display.py
+++ esphome/components/HUB75/display.py
@@ -1,16 +1,18 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
-from esphome.components import display, spi
+from esphome.components import display
 from esphome.const import (
     CONF_ID,
-    CONF_ENABLE_PIN,
     CONF_WIDTH,
-    CONF_BIT_DEPTH,
     CONF_LAMBDA,
     CONF_PAGES,
-    CONF_BRIGHTNESS
+    CONF_CLOCK_PIN,
+    CONF_ENABLE_PIN,
+    CONF_BRIGHTNESS,
 )
+CONF_LATCH_PIN = "latch_pin"
+
 CONF_DOUBLE_BUFFER = "double_buffer"
 
 CONF_COLOR_R1 = "R1 Color Pin"
@@ -24,7 +26,7 @@ CONF_ADDRESS_B = "Address B Pin"
 CONF_ADDRESS_C = "Address C Pin"
 CONF_ADDRESS_D = "Address D Pin"
 
-DEPENDENCIES = ["spi"]
+# DEPENDENCIES = ["spi"]
 
 CODEOWNERS = ["@IJIJI"]
 
@@ -39,10 +41,8 @@ CONFIG_SCHEMA = cv.All(
         {
             cv.GenerateID(): cv.declare_id(HUB75),
             cv.Required(CONF_ENABLE_PIN): pins.gpio_output_pin_schema,
-            cv.Required(CONF_BRIGHTNESS): pins.gpio_output_pin_schema,
-            cv.Required(CONF_WIDTH): pins.gpio_output_pin_schema,
-            cv.Required(CONF_BIT_DEPTH): pins.gpio_output_pin_schema,
-            cv.Required(CONF_DOUBLE_BUFFER): pins.gpio_output_pin_schema,
+            cv.Required(CONF_CLOCK_PIN): pins.gpio_output_pin_schema,
+            cv.Required(CONF_LATCH_PIN): pins.gpio_output_pin_schema,
 
             cv.Required(CONF_COLOR_R1): pins.gpio_output_pin_schema,
             cv.Required(CONF_COLOR_G1): pins.gpio_output_pin_schema,
@@ -55,47 +55,65 @@ CONFIG_SCHEMA = cv.All(
             cv.Required(CONF_ADDRESS_C): pins.gpio_output_pin_schema,
             cv.Required(CONF_ADDRESS_D): pins.gpio_output_pin_schema,
 
+            cv.Optional(CONF_WIDTH, default=64): cv.int_,
+            # TODO MAKE RANGE
+            cv.Optional(CONF_BRIGHTNESS, default=255, ): cv.int_,
+            cv.Optional(CONF_DOUBLE_BUFFER, default=False): cv.boolean,
+            cv.Optional(CONF_ROTATION, default=0): cv.Range(min=0, max=3),
         }
     )
-    .extend(cv.polling_component_schema("1s"))
-    .extend(spi.spi_device_schema()),
+    .extend(cv.polling_component_schema("1s")),
+    # .extend(spi.spi_device_schema()),
     cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA),
 )
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
 
-    await display.register_display(var, config)
-    await spi.register_spi_device(var, config)
 
+    addr_a = await cg.gpio_pin_expression(config[CONF_ADDRESS_A])
+    addr_b = await cg.gpio_pin_expression(config[CONF_ADDRESS_B])
+    addr_c = await cg.gpio_pin_expression(config[CONF_ADDRESS_C])
+    addr_d = await cg.gpio_pin_expression(config[CONF_ADDRESS_D])
+
+    clock = await cg.gpio_pin_expression(config[CONF_CLOCK_PIN])
+    latch = await cg.gpio_pin_expression(config[CONF_LATCH_PIN])
     enable = await cg.gpio_pin_expression(config[CONF_ENABLE_PIN])
-    cg.add(var.set_oe_pin(enable))
+
+    width = config[CONF_WIDTH]
 
     col_r1 = await cg.gpio_pin_expression(config[CONF_COLOR_R1])
-    cg.add(var.set_col_r1_pin(col_r1))
     col_g1 = await cg.gpio_pin_expression(config[CONF_COLOR_G1])
-    cg.add(var.set_col_g1_pin(col_g1))
     col_b1 = await cg.gpio_pin_expression(config[CONF_COLOR_B1])
-    cg.add(var.set_col_b1_pin(col_b1))
     col_r2 = await cg.gpio_pin_expression(config[CONF_COLOR_R2])
-    cg.add(var.set_col_r1_pin(col_r2))
     col_g2 = await cg.gpio_pin_expression(config[CONF_COLOR_G2])
-    cg.add(var.set_col_g1_pin(col_g2))
     col_b2 = await cg.gpio_pin_expression(config[CONF_COLOR_B2])
-    cg.add(var.set_col_b1_pin(col_b2))
-    
-    addr_a = await cg.gpio_pin_expression(config[CONF_ADDRESS_A])
-    cg.add(var.set_addr_a_pin(addr_a))
-    addr_b = await cg.gpio_pin_expression(config[CONF_ADDRESS_B])
-    cg.add(var.set_addr_b_pin(addr_b))
-    addr_c = await cg.gpio_pin_expression(config[CONF_ADDRESS_C])
-    cg.add(var.set_addr_c_pin(addr_c))
-    addr_d = await cg.gpio_pin_expression(config[CONF_ADDRESS_D])
-    cg.add(var.set_addr_d_pin(addr_d))
 
+    dual_buffer = config[CONF_DOUBLE_BUFFER]
+
+    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(
+        config[CONF_ID],
+        addr_a,
+        addr_b,
+        addr_c,
+        addr_d,
+        clock,
+        latch,
+        enable,
+        width,
+        [col_r1, col_g1, col_b1, col_r2, col_g2, col_b2],
+        dual_buffer,
+    )
+
+    cg.add(var.begin())
+
+    await display.register_display(var, config)
+    # await spi.register_spi_device(var, config)
 
     cg.add(var.set_brightness(config[CONF_BRIGHTNESS]))
+
+
 
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
